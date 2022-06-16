f1b2f09263014856b9603bd38a3742b69f0bbd96
Dakshitha mevan Dias <dakshithadias@gmail.com>
2022-06-16 11:42:49 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,11 +2,12 @@
 echo "this is inside build file"
 echo "creating new directory..."
 mkdir newdir
+echo $PROJECT_NAME
 echo "copies readme file to new directory....."
 cp -v readme.txt newdir/
 echo "contents of workspace....."
 ls
 echo "\n contents of new directory....."
 ls newdir/
-sed -i 's/test/"$PROJECT_NAME"/g' jeevani.txt
+sed -i 's/test/$PROJECT_NAME/g' jeevani.txt
 cat jeevani.txt
